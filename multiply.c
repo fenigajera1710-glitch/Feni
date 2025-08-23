1d359ece1bd0bca84f00d1d94602a55be6283569
fenigajera1710-glitch <fenigajera1710@gmail.com>
2025-08-23 11:27:56 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,multiply;
+
+    printf("enter first number:");
+    scanf("%d",&num1);
+
+    printf("enter second number:");
+    scanf("%d",&num2);
+
+    multiply = num1*num2;
+    printf("multiply = %d\n",multiply);
+
+    return 0;
+
+}
