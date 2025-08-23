9bcb755c25862804ffe7759ca67e7b01b02c5049
fenigajera1710-glitch <fenigajera1710@gmail.com>
2025-08-23 21:17:14 +0530

    Add files via upload

---
+++ FintoC.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    float celsius,fahrenheit;
+
+    printf("Enter temperature in fahrenheit:");
+    scanf("%f",&fahrenheit);
+
+    celsius = (fahrenheit - 32) * 5.0/9.0;   
+
+    printf("%.2f Fahrenheit = %.2f Celsius\n",fahrenheit,celsius);
+
+    return 0;
+}
