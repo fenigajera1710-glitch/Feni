f99fe0c0228e20be3e43218259968a8df5cce1ac
fenigajera1710-glitch <fenigajera1710@gmail.com>
2025-09-27 19:59:04 +0530

    Add files via upload

---
+++ togglecase.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[100];
+    printf("Enter a string:");
+    gets(str);
+
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;
+        }
+        else if(str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+        }
+    }
+
+    printf("Toggle case string:%s\n",str);
+
+    return 0;
+}
