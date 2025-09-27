55464042180e18d2a72aed45746681701fab10e4
fenigajera1710-glitch <fenigajera1710@gmail.com>
2025-09-27 20:11:06 +0530

    Add files via upload

---
+++ copystring.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+
+    char str1[100], str2[200];
+    
+    printf("Enter string:");
+    gets(str1);
+
+    strcpy(str2,str1);
+
+    printf("Original string: %s\n",str1);
+    printf("Copied string: %s\n",str2);
+
+    return 0;
+}
