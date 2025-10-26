008f2e881393d86afd7c7b8680c2d28270da5b0a
fenigajera1710-glitch <fenigajera1710@gmail.com>
2025-10-26 04:15:41 -0700

    Add files via upload

---
+++ frequencyofvowels.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char str[100];
+    int a = 0 ,e = 0 ,i = 0 ,o = 0 , u = 0 ;
+    printf("Enter a string:");
+    gets(str);
+
+    for(int j = 0; str[j] != '\0'; j++){
+        switch(str[j]){
+            case 'a':
+            case 'A':  a++; break;
+            case 'e':
+            case 'E':  e++; break;
+            case 'i':
+            case 'I':  i++; break;
+            case 'o':
+            case 'O':  o++; break;
+            case 'u':
+            case 'U':  u++; break;
+        }
+    }
+    printf("Fequency of vowels:\n");
+    printf("a/A = %d\n",a);
+    printf("e/E = %d\n",e);
+    printf("i/I = %d\n",i);
+    printf("o/O = %d\n",o);
+    printf("u/U = %d\n",u);
+
+
+    return 0;
+}
